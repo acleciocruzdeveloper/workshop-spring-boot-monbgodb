66085608ced6992605b879a84b5cecc810878ea5
Aclecio Cruz <aclecioscruz@gmail.com>
2021-10-18 10:42:44 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/acleciocruz/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.acleciocruz.workshopmongo.repository;
+
+import com.acleciocruz.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
