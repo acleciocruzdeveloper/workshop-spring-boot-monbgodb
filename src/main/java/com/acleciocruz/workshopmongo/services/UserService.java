c91cfc294f23ad992d81615f13d8a1dd266e194e
Aclecio Cruz <aclecioscruz@gmail.com>
2021-10-18 13:32:27 -0300

    Inserção de usuário com POST

--- src/main/java/com/acleciocruz/workshopmongo/services/UserService.java
+++ src/main/java/com/acleciocruz/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.acleciocruz.workshopmongo.services;
 
 import com.acleciocruz.workshopmongo.domain.User;
+import com.acleciocruz.workshopmongo.dto.UserDTO;
 import com.acleciocruz.workshopmongo.repository.UserRepository;
 import com.acleciocruz.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,12 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repo.findById ( id );
         return obj.orElseThrow (() -> new ObjectNotFoundException ( "Objeto não encontrado" ));
+    }
+
+    public User insert(User obj){
+        return repo.insert ( obj );
+    }
+    public User fromDTO(UserDTO objDto){
+        return new User (objDto.getId (), objDto.getName (), objDto.getEmail ());
     }
 }
